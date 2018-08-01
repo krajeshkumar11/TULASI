77b964964ab72965b39ecc722d28de0c5f114d95
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-01 17:30:32 +0530

    Edited submission.

--- cspp1-practice/m3/helloworld.py
+++ cspp1-practice/m3/helloworld.py
@@ -1,9 +1,4 @@
-# My first program was to print the string "hello world"
-
-list = ["h","e","l","l","o"," ","w","o","r","l","d"]
-i = 1
-String = ""
-while i <= len(list):
-    string += string + list[i]
-
-print(string)
+"""
+Basic Program
+"""
+print("hello world")
