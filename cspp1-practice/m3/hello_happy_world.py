292ec9238f18908c32c6b9722bc8e6cd71f0034a
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-01 17:31:55 +0530

    First Submission

---
+++ cspp1-practice/m3/hello_happy_world.py
@@ -0,0 +1,9 @@
+"""
+Basic Program
+"""
+HAPPY = int(input("Enter only integer:"))
+if HAPPY > 2:
+    print("hello world")
+else:
+    print("Your input should be greater than 2 for a required output.")
+	
