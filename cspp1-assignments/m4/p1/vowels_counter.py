8a1afa2a542edd0cd0cf3858b2afddc4fb829d21
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-02 14:59:23 +0530

    " -> 4 of 4 passed. pylint: 1.25/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -2,7 +2,7 @@
 #Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
 #Number of vowels: 5
 def main():
-    var_input = input("Enter the string in lowercase only:")  # THIS IS STEP FOR ASKING USER INPUT.
+    var_input = input()  # THIS IS STEP FOR ASKING USER INPUT.
     var_count = 0   # THIS VARIABLE IS USED TO KEEP THE COUNT OF VOWELS
     for var_character in var_input:
         if var_character == "a" or var_character == "e" or var_character == "i" or var_character == "o" or var_character == "u":   # THIS LINE IS TO CHECK WHETHER THERE ARE ANY VOWELS. 
