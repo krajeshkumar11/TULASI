0b6718dfeafaf711043a6b8ad311f53a79ad6832
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-04 11:16:17 +0530

    " -> 7 of 7 passed. pylint: 9.41/10 "

--- cspp1-assignments/m6/p2/special_char.py
+++ cspp1-assignments/m6/p2/special_char.py
@@ -9,13 +9,17 @@ def main():
     '''
     str_input = input()
     str_output = ""
-    array_check = ["!","@","#","$","%","^","&","*"]
+    array_check = ["!", "@", "#", "$", "%", "^", "&", "*"]
+    temp_i = 0
     for i in range(len(str_input)):
-        temp_i = i
+        flag = 0
         if str_input[i] in array_check:
-            str_output += str_input[:i] + " " + str_input[i + 1:]
-            
+            str_output += str_input[temp_i:i] + " "
+            flag = 1
+        if flag == 1:
+            temp_i = i + 1
+    if len(str_output) != len(str_input):
+        str_output += str_input[temp_i:]
     print(str_output)
-
 if __name__ == "__main__":
     main()
