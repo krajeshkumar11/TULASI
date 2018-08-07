0c80890885d9f75c260eeda1230791a6b52b05db
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-07 23:53:43 +0530

    " -> 8 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
@@ -10,31 +10,31 @@ the debt within a year. Try it out with
 # large inputs, and notice how fast it is. Produce the same return
 # value as you did in Assignment 2.
 """
-def var_payingDebtOffInAYear(var_balance, var_annualInterestRate):
+def var_payingdebtoffinayear(var_balance, var_annualinterestrate):
     """
     This function is using bi-section search
     """
-    var_monthlyinterestrate = (var_annualInterestRate) / 12.0
+    var_monthlyinterestrate = (var_annualinterestrate) / 12.0
     var_monthlypaymentlowerbound = var_balance / 12
     var_monthlypaymentupperbound = (var_balance * (1 + var_monthlyinterestrate)**12) / 12.0
     var_newbalance = var_balance
     var_epsilon = 0.0001
-    var_guess = (monthly_payment_lower_bound + monthly_payment_upper_bound)/2
+    var_guess = (var_monthlypaymentlowerbound + var_monthlypaymentupperbound)/2
     while True:
         var_month = 1
         while var_month <= 12:
             var_newbalance = var_newbalance - var_guess
-            var_newbalance = new_balance + (monthly_interest_rate * new_balance)
-            month += 1
-        if new_balance > 0 and new_balance > epsilon:
-            monthly_payment_lower_bound = guess
-            new_balance = balance
-        elif new_balance < 0 and new_balance < -epsilon:
-            monthly_payment_upper_bound = guess
-            new_balance = balance
+            var_newbalance = var_newbalance + (var_monthlyinterestrate * var_newbalance)
+            var_month += 1
+        if var_newbalance > 0 and var_newbalance > var_epsilon:
+            var_monthlypaymentlowerbound = var_guess
+            var_newbalance = var_balance
+        elif var_newbalance < 0 and var_newbalance < -var_epsilon:
+            var_monthlypaymentupperbound = var_guess
+            var_newbalance = var_balance
         else:
-            return guess
-        guess = (monthly_payment_lower_bound + monthly_payment_upper_bound)/2
+            return var_guess
+        var_guess = (var_monthlypaymentlowerbound + var_monthlypaymentupperbound)/2
 def main():
     """
     This function is to call the above
@@ -42,6 +42,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: " + str(round(payingDebtOffInAYear(data[0], data[1]), 2)))
+    print("Lowest Payment: " + str(round(var_payingdebtoffinayear(data[0], data[1]), 2)))
 if __name__ == "__main__":
     main()
