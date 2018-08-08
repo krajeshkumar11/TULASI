46863422bee2a5423a69120e65e5b89bc7753336
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-08 20:07:59 +0530

    " -> 6 of 9 passed. pylint: 10.00/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -14,9 +14,9 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-    for letter in secret_word:
-        if char in letters_guessed:
-            secret_word.replace(char, "")
+    for i in secret_word:
+        if i in letters_guessed:
+            secret_word.replace(i, "")
     if secret_word == "":
         return True
     return False
