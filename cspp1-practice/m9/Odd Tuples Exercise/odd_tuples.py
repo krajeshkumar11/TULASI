74100233c355645bf6e172f0a068dfd5a10faa8d
TulasiJagan49 <jaganmeruga777@msitprogram.net>
2018-08-08 12:59:46 +0530

    M9-A3

---
+++ cspp1-practice/m9/Odd Tuples Exercise/odd_tuples.py
@@ -0,0 +1,32 @@
+#Exercise : Odd Tuples
+#Write a python function oddTuples(aTup) that takes a some numbers in the tuple as input and returns a tuple in which contains odd index values in the input tuple  
+
+
+
+def oddTuples(aTup):
+    '''
+    aTup: a tuple
+    
+    returns: tuple, every other element of aTup. 
+    '''
+    bTup = ()
+    i = 0
+    while i < len(aTup):
+    	if i % 2 == 0:
+    		bTup += (aTup[i],)
+    	i = i + 1
+    return (bTup)
+
+    
+
+def main():
+    data = input()
+    data = data.split()
+    aTup=()
+    for j in range(len(data)):
+        aTup += (int(data[j]),)
+    print(oddTuples(aTup))
+        
+
+if __name__ == "__main__":
+    main()
